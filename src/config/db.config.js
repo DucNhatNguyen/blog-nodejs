37cd494af0e33b429708b7ab350c7db19b281e6b
DESKTOP-S5G05N0\nguyw <nguyw461@gmail.com>
2023-02-10 22:45:15 +0700

    change configdb

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -3,6 +3,7 @@ module.exports = {
 	USER: 'nguyw461',
 	PASSWORD: 'Cc72eE8OyQT5TC4kby4LEV52bVWkzhkT',
 	DB: 'neondb',
+	port: 5432,
 	dialect: 'postgres',
 	sslmode: 'no-verify',
 	pool: {
@@ -11,4 +12,7 @@ module.exports = {
 		acquire: 300000,
 		idle: 300000,
 	},
+	idle_timeout: 6000,
+	keepAlive: true,
+	secret: 'crypto',
 }
