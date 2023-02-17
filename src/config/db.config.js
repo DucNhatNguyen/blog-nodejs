767d3c2516b1b16f07f4d7b160d1e6e9168f9f2f
DESKTOP-S5G05N0\nguyw <nguyw461@gmail.com>
2023-02-17 22:38:01 +0700

    change db config

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	HOST: 'dpg-cem6bmun6mpkfa6e3k2g-a.singapore-postgres.render.com',
+	HOST: 'ep-shrill-snow-006772.ap-southeast-1.aws.neon.tech',
 	USER: 'nguyw461',
-	PASSWORD: 'Cc72eE8OyQT5TC4kby4LEV52bVWkzhkT',
+	PASSWORD: 't3zwPD1gNhEC',
 	DB: 'neondb',
 	port: 5432,
 	dialect: 'postgres',
@@ -16,3 +16,22 @@ module.exports = {
 	keepAlive: true,
 	secret: 'crypto',
 }
+
+// module.exports = {
+// 	HOST: 'dpg-cem6bmun6mpkfa6e3k2g-a.singapore-postgres.render.com',
+// 	USER: 'nguyw461',
+// 	PASSWORD: 'Cc72eE8OyQT5TC4kby4LEV52bVWkzhkT',
+// 	DB: 'neondb',
+// 	port: 5432,
+// 	dialect: 'postgres',
+// 	sslmode: 'no-verify',
+// 	pool: {
+// 		max: 64,
+// 		min: 2,
+// 		acquire: 300000,
+// 		idle: 300000,
+// 	},
+// 	idle_timeout: 6000,
+// 	keepAlive: true,
+// 	secret: 'crypto',
+// }
